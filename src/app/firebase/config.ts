d3239415c730e3950caa338f550cb938c2d5a29a
BrendanD7 <brendand7@hotmail.com>
2024-04-23 18:22:04 -0300

    Update config.ts

--- src/app/firebase/config.ts
+++ src/app/firebase/config.ts
@@ -1,7 +1,7 @@
 import { initializeApp, getApps } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    apiKey: "AIzaSyCFVTLR7Yf-Wp7gIqAo1bERbW7U0I16HZw",
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
